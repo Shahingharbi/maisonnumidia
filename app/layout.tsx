c4356eb52d91e5ac3841c74aaecbaee3fb65db55
sgharbi <shahin.gharbi@superindep.fr>
2026-03-25 18:30:10 +0100

    ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -50,6 +51,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-77YXRM3HBT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-77YXRM3HBT');
+          `}
+        </Script>
+      </head>
       <body className={`${poppins.variable} ${inter.variable} antialiased`}>
         <script
           type="application/ld+json"
